016b0c6e081ff6faef245c2b6ecc4b63d72316ae
rrerick <78693116+rrerick@users.noreply.github.com>
2022-07-05 15:17:27 -0300

    Update mergeall

--- mergeall.sh
+++ mergeall.sh
@@ -2,7 +2,7 @@
 
 for file in $(find ~/jjfast/brasil/*shp); do
     echo $file
-    #unzip $file -d ~/jjfast/brasil/
+    unzip $file -d ~/jjfast/brasil/
     ogr2ogr -f 'GPKG' -update -append ~/jjfast/brasil_deforestation.gpkg $file
 done;
 
